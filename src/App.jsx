6d7e267b5daaa7e268b5349061bdc8b6d40539f0
Elhamudin Sameem <elhamsmacbookpro@elhamudins-mbp.myfiosgateway.com>
2023-09-13 23:21:30 -0400

    feat: added NavBar and fetch all products in app level component. Added styling to product divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Cart from "./components/Cart.jsx";
 import "./App.css";
+import { fetchAllProducts } from "./API/api.js";
 
 function App() {
-  return <></>;
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    async function getAllProducts() {
+      const result = await fetchAllProducts();
+      setProducts(result);
+    }
+    getAllProducts();
+  }, []);
+  console.log(products);
+
+  return (
+    <>
+      <NavBar />
+      <div className="canvas">
+        <Routes>
+          <Route path="/" element={<Home products={products} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
