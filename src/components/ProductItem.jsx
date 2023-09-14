338481df7039390d4290a5103aac22226c2561d1
Elhamudin Sameem <elhamsmacbookpro@Elhamudins-MacBook-Pro.local>
2023-09-14 08:22:13 -0400

    feat: added onClick attribute to each product div to view that single product

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../style/ProductItem.css";
 
 function ProductItem({ product }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="product-div">
+    <div
+      className="product-div"
+      onClick={() => {
+        navigate(`/products/${product.id}`);
+      }}
+      title="View Product"
+    >
       <div className="product-title">
         <h3>{product.title}</h3>
         <h2 className="product-price">${product.price}</h2>
