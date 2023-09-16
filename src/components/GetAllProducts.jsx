39a5ebec2275041512919d5e6e6d88344bdb834c
Elhamudin Sameem <elhamsmacbookpro@elhamudins-mbp.myfiosgateway.com>
2023-09-16 19:54:35 -0400

    feat: added loading message and stlying to home page and login page.

--- src/components/GetAllProducts.jsx
+++ src/components/GetAllProducts.jsx
@@ -24,89 +24,96 @@ function GetAllProducts({ products, loading }) {
 
   return (
     <>
-      <div className="search-div">
-        <label htmlFor="search-product-input">
-          <b>Search</b>{" "}
-        </label>
-        <input
-          type="text"
-          id="search-product-input"
-          name="search-product-input"
-          placeholder=" Anything"
-          value={searchedProduct}
-          onChange={(e) => {
-            setSearchedProduct(e.target.value);
-          }}
-        />
-      </div>
-      <div className="category">
-        <button
-          className={`cat-button ${category === "all" ? "active-button" : ""}`}
-          onClick={() => {
-            setTitleText("All Products");
-            setCategory("all");
-          }}
-        >
-          All
-        </button>
-        <button
-          className={`cat-button ${
-            category === "men's clothing" ? "active-button" : ""
-          }`}
-          onClick={() => {
-            setTitleText("Men's");
-            setCategory(`men's clothing`);
-          }}
-        >
-          Men
-        </button>
-        <button
-          className={`cat-button ${
-            category === "women's clothing" ? "active-button" : ""
-          }`}
-          onClick={() => {
-            setTitleText("Women's");
-            setCategory(`women's clothing`);
-          }}
-        >
-          Women
-        </button>
-        <button
-          className={`cat-button ${
-            category === "jewelery" ? "active-button" : ""
-          }`}
-          onClick={() => {
-            setTitleText("Jewelery");
-            setCategory(`jewelery`);
-          }}
-        >
-          Jewelery
-        </button>
-        <button
-          className={`cat-button ${
-            category === "electronics" ? "active-button" : ""
-          }`}
-          onClick={() => {
-            setTitleText("Electronics");
-            setCategory(`electronics`);
-          }}
-        >
-          Electronics
-        </button>
-      </div>
-      <h1 className="cat-title">{titleText}</h1>
       {loading ? (
-        <h3 className="loading">Loading...</h3>
+        <h2 className="loading">LOADING...</h2>
       ) : (
-        <div className="all-products-div">
-          {filteredProducts.length > 0 ? (
-            filteredProducts.map((product) => {
-              return <ProductItem key={product.id} product={product} />;
-            })
-          ) : (
-            <h1>Out of inventory</h1>
-          )}
-        </div>
+        <>
+          <h1 className="cat-title">{titleText}</h1>
+          <div className="filter-div">
+            <div className="search-div">
+              <label htmlFor="search-product-input">
+                <b>Search</b>{" "}
+              </label>
+              <input
+                type="text"
+                id="search-product-input"
+                name="search-product-input"
+                placeholder=" Anything"
+                value={searchedProduct}
+                onChange={(e) => {
+                  setSearchedProduct(e.target.value);
+                }}
+              />
+            </div>
+            <div className="category">
+              <button
+                className={`cat-button ${
+                  category === "all" ? "active-button" : ""
+                }`}
+                onClick={() => {
+                  setTitleText("All Products");
+                  setCategory("all");
+                }}
+              >
+                All
+              </button>
+              <button
+                className={`cat-button ${
+                  category === "men's clothing" ? "active-button" : ""
+                }`}
+                onClick={() => {
+                  setTitleText("Men's");
+                  setCategory(`men's clothing`);
+                }}
+              >
+                Men
+              </button>
+              <button
+                className={`cat-button ${
+                  category === "women's clothing" ? "active-button" : ""
+                }`}
+                onClick={() => {
+                  setTitleText("Women's");
+                  setCategory(`women's clothing`);
+                }}
+              >
+                Women
+              </button>
+              <button
+                className={`cat-button ${
+                  category === "jewelery" ? "active-button" : ""
+                }`}
+                onClick={() => {
+                  setTitleText("Jewelery");
+                  setCategory(`jewelery`);
+                }}
+              >
+                Jewelery
+              </button>
+              <button
+                className={`cat-button ${
+                  category === "electronics" ? "active-button" : ""
+                }`}
+                onClick={() => {
+                  setTitleText("Electronics");
+                  setCategory(`electronics`);
+                }}
+              >
+                Electronics
+              </button>
+            </div>
+          </div>
+
+          <div className="all-products-div">
+            {filteredProducts.length > 0 ? (
+              filteredProducts.map((product) => {
+                return <ProductItem key={product.id} product={product} />;
+              })
+            ) : (
+              <h1>Out of inventory</h1>
+            )}
+          </div>
+        </>
       )}
     </>
   );
