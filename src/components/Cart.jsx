4a974c6928018e5e14bd7b71587127e17d91d2b2
Elham Sameem <126508470+elhamsameem@users.noreply.github.com>
2023-09-27 18:04:50 -0400

    feat: added logic to display cart is empty if user cart is empty

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -44,59 +44,76 @@ export default function Cart({ cart, setCart }) {
   return (
     <div className="cart-canvas">
       {cart.length > 0 ? (
-        cart.map((item) => {
-          return (
-            <div className="cart-item-div" key={item.id}>
-              <div className="cart-item-img-div">
-                <img
-                  src={item.image}
-                  className="cart-item-img"
+        <>
+          {cart.map((item) => {
+            return (
+              <div className="cart-item-div" key={item.id}>
+                <div className="cart-item-img-div">
+                  <img
+                    src={item.image}
+                    className="cart-item-img"
+                    onClick={() => openItem(item.id)}
+                  />
+                </div>
+                <h4
+                  className="cart-item-title"
                   onClick={() => openItem(item.id)}
-                />
-              </div>
-              <h4 className="cart-item-title" onClick={() => openItem(item.id)}>
-                {item.title}
-              </h4>
-              <p className="cart-item-price">${item.price}</p>
-              <div className="cart-item-qty-div">
-                QTY:
-                <button
-                  className="increament-button qty-buttons"
-                  onClick={() => {
-                    increaseQty(item);
-                  }}
                 >
-                  +
-                </button>
-                <p className="item-qty">{item.quantity}</p>
-                <button
-                  className="decreament-button qty-buttons"
-                  onClick={() => {
-                    decreaseQty(item);
-                  }}
-                >
-                  -
+                  {item.title}
+                </h4>
+                <p className="cart-item-price">${item.price}</p>
+                <div className="cart-item-qty-div">
+                  Qty:
+                  <button
+                    className="increament-button qty-buttons"
+                    onClick={() => {
+                      increaseQty(item);
+                    }}
+                  >
+                    +
+                  </button>
+                  <p className="item-qty">{item.quantity}</p>
+                  <button
+                    className="decreament-button qty-buttons"
+                    onClick={() => {
+                      decreaseQty(item);
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+                <div className="button-div">
+                  <button
+                    className="remove-button"
+                    onClick={() => {
+                      const newItems = cart.filter(
+                        (product) => item.id !== product.id
+                      );
+                      setCart(newItems);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
+                <h4 className="cart-item-subtotal">
+                  Total: ${(item.price * item.quantity).toFixed(2)}
+                </h4>
+              </div>
+            );
+          })}
+          <div className="checkout-div">
+            <div className="checkout-button-div">
+              <div className="checkout-button-div2">
+                <button className="simple-button checkout-button">
+                  Procceed to Checkout
                 </button>
               </div>
-              <div className="button-div">
-                <button
-                  className="remove-button"
-                  onClick={() => {
-                    const newItems = cart.filter(
-                      (product) => item.id !== product.id
-                    );
-                    setCart(newItems);
-                  }}
-                >
-                  Remove
-                </button>
-              </div>
-              <h4 className="cart-item-subtotal">
-                Total: ${(item.price * item.quantity).toFixed(2)}
-              </h4>
             </div>
-          );
-        })
+            <div className="grand-total-div">
+              <h3 className="grand-total">{`Grand Total: $9865498`}</h3>
+            </div>
+          </div>
+        </>
       ) : (
         <h1>Cart is Empty</h1>
       )}
